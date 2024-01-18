5904c02649a9f454bc98cc354c985f1c658d432e
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-01-18 22:08:59 +0530

    Basic app setup

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+        <div className="HomeComponent">
+            <div className="HomeComponent-in">
+                <h1>Sri</h1>
+            </div>
+        </div>
+  )
+}
